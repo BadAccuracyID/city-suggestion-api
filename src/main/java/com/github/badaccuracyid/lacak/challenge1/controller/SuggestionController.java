4a0f8db7df1a748cfc648626a028a775a043746e
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-11-22 19:27:01 +0700

    feat(Suggestion API): return response entity

--- src/main/java/com/github/badaccuracyid/lacak/challenge1/controller/SuggestionController.java
+++ src/main/java/com/github/badaccuracyid/lacak/challenge1/controller/SuggestionController.java
@@ -2,6 +2,7 @@ package com.github.badaccuracyid.lacak.challenge1.controller;
 
 import com.github.badaccuracyid.lacak.challenge1.dto.SuggestionResponse;
 import com.github.badaccuracyid.lacak.challenge1.service.SuggestionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class SuggestionController {
     }
 
     @GetMapping("/suggestions")
-    public SuggestionResponse suggestions(
+    public ResponseEntity<SuggestionResponse> suggestions(
             @RequestParam String q,
             @RequestParam(required = false) Double latitude,
             @RequestParam(required = false) Double longitude
     ) {
-        return suggestionService.getSuggestions(q, latitude, longitude);
+        SuggestionResponse response = suggestionService.getSuggestions(q, latitude, longitude);
+        return ResponseEntity.ok(response);
     }
+
 }
