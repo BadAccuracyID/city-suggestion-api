f369de0e115004016072a9d20b376f4770fd492e
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-11-22 18:52:36 +0700

    fix(CityRepository): fix wrong extend param

--- src/main/java/com/github/badaccuracyid/lacak/challenge1/repository/CityRepository.java
+++ src/main/java/com/github/badaccuracyid/lacak/challenge1/repository/CityRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CityRepository extends JpaRepository<Long, City> {
+public interface CityRepository extends JpaRepository<City, Long> {
 
     @Query(value = "SELECT c FROM City c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :query, '%'))")
     List<City> findByNameContainingIgnoreCase(@Param("query") String query);
