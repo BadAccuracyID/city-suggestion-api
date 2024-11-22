70ed94442f4fd23932020d4f070da8f631900f30
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-11-22 18:06:02 +0700

    Initial commit

---
+++ src/main/java/com/github/badaccuracyid/lacak/challenge1/LacakCodingChallenge1Application.java
@@ -0,0 +1,13 @@
+package com.github.badaccuracyid.lacak.challenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacakCodingChallenge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacakCodingChallenge1Application.class, args);
+	}
+
+}
