82bf871bb0cbfcb50f4d04ac54f3d0bc69631bba
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-11-24 22:01:24 +0700

    feat: add more exception handlers

--- src/main/java/com/github/badaccuracyid/lacak/challenge1/advice/GlobalExceptionHandler.java
+++ src/main/java/com/github/badaccuracyid/lacak/challenge1/advice/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Hidden
 @RestControllerAdvice
@@ -35,7 +37,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleInternalServerError(Exception ex) {
         log.error("Internal server error: ", ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("An unexpected error occurred."));
+        String errorMessage = "An unexpected error occurred.";
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        String errorMessage = "The requested URL is invalid or the HTTP method is not supported.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NoHandlerFoundException ex) {
+        String errorMessage = "The requested URL was not found on the server. Please check your endpoint.";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(errorMessage));
     }
 
 }
---
+++ src/main/java/com/github/badaccuracyid/lacak/challenge1/configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package com.github.badaccuracyid.lacak.challenge1.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+
+}
